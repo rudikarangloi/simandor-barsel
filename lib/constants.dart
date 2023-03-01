ed4aadada1ef6cd54efff7dfc274b52dc2b80a5e
rudicatsmile <rudi.catsmile2@gmail.com>
2023-03-01 23:36:48 +0700

    variabel di letakan pada constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 //For developmet on localhost
-//const String BASE_URL = "http://192.168.1.201:81/BALANGAN/sicaper/api";
-// const String BASE_URL = "http://103.180.125.35/api";
+const String BASE_URL = "http://192.168.1.6:81/SimBADA_2020/api";
 // const String BASE_URL_IMAGES = "http://192.168.43.23:81/sipanda-bmd/images/";
 // const String BASE_URL_IMAGES_SIMANDOR =
 //     "http://192.168.43.23:81/sipanda-bmd/admin/simandor/images";
-const String BASE_URL = "https://sicaper.balangankab.go.id/api";
+
+//const String BASE_URL = "https://simbada2020.baritoselatankab.go.id/api";
 const String BASE_URL_IMAGES = "https://sipandabmd.balangankab.go.id/images";
 const String BASE_URL_IMAGES_SIMANDOR =
     "https://sipandabmd.balangankab.go.id/admin/simandor/images";
+
+const applicationStr = "Aplikasi";
+const applicationName = "SIMANDOR";
+const clientAppName = "Kabupaten Barito Selatan";
 
 const kBackgroundColor = Color(0xFFF8F8F8);
 const kActiveIconColor = Color(0xFFE68342);
